ac5ab5a90a371ef719a3f10cc97c430ce44cadc4
pro <pro@prodeMacBook-Pro-2.local>
2023-08-26 00:54:43 +0600

    create a crispy form with bootstrap

--- book/views.py
+++ book/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from book.forms import BookStoreForm
 # Create your views here.
 
 
 def home(request):
     return render(request, 'store_book.html')
+
+
+def store_book(request):
+    book = BookStoreForm(request.POST)
+    print(book)
+    return render(request, 'store_book.html', {'form': book})
