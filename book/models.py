d90c8c573e02db241e98f820ecb537f1bca1ce32
pro <pro@prodeMacBook-Pro-2.local>
2023-08-27 10:54:35 +0600

    modify admin panel

--- book/models.py
+++ book/models.py
@@ -15,5 +15,5 @@ class BookStoreModel(models.Model):
     book_name = models.CharField(max_length=30)
     author = models.CharField(max_length=30)
     category = models.CharField(max_length=30, choices=CATEGORY)
-    first_pub = models.DateTimeField(auto_now_add=True)
-    last_pub = models.DateTimeField(auto_now=True)
+    first_pub = models.DateTimeField(auto_now_add=True)  # first date
+    last_pub = models.DateTimeField(auto_now=True)  # update date
